36df1d7ae675244dc9904637eb7e8d5c4e97c084
goia <frnnd.goia97@gmail.com>
2024-08-16 15:18:13 -0300

    Testing input component

--- imhere/src/screens/Home/index.tsx
+++ imhere/src/screens/Home/index.tsx
@@ -1,12 +1,18 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, TextInput, View } from "react-native";
 import { styles } from "./styles";
 
-export default function Home() {
+export function Home() {
   return (
-    <View style={{ flex: 1, backgroundColor: "#131016", padding: 24 }}>
-      <Text style={styles.container}>Nome do evento</Text>
-      <Text style={styles.eventName}>Sexta, 4 de Novembro de 2022.</Text>
+    <View style={styles.container}>
+      <Text style={styles.eventName}>Nome do evento</Text>
+      <Text style={styles.eventDate}>Sexta, 4 de Novembro de 2022.</Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder="Nome do participante"
+        placeholderTextColor={"#6b6b6b"}
+      />
     </View>
   );
 }
--- imhere/src/screens/Home/styles.ts
+++ imhere/src/screens/Home/styles.ts
@@ -2,13 +2,26 @@ import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
   container: {
+    flex: 1,
+    backgroundColor: "#131016",
+    padding: 24,
+  },
+  eventName: {
     color: "#fdfcfe",
     fontSize: 24,
     fontWeight: "bold",
     marginTop: 48,
   },
-  eventName: {
+  eventDate: {
     color: "#6b6b6b",
+    fontSize: 16,
+  },
+  input: {
+    backgroundColor: "#1f1e25",
+    height: 56,
+    borderRadius: 5,
+    color: "#fdfcfe",
+    padding: 16,
     fontSize: 16,
   },
 });
