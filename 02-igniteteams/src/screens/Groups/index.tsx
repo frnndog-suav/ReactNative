c94fcdc69b97cad00300587b8b72de1715e61d1c
goia <frnnd.goia97@gmail.com>
2024-10-22 15:54:46 -0300

    Implementing groups list

--- 02-igniteteams/src/screens/Groups/index.tsx
+++ 02-igniteteams/src/screens/Groups/index.tsx
@@ -1,17 +1,25 @@
-import { Header } from "@components/Header";
-import { Container } from "./styles";
-import { Highlight } from "@components/Highlight";
 import { GroupCard } from "@components/GroupCard";
+import { Header } from "@components/Header";
+import { Highlight } from "@components/Highlight";
+import { useState } from "react";
+import { FlatList } from "react-native";
+import { Container } from "./styles";
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>([
+    "Galera da Rocketseat",
+    "Amigos",
+  ]);
+
   return (
     <Container>
       <Header />
       <Highlight title="Turmas" subtitle="Jogue com sua turma" />
 
-      <GroupCard
-        title="Galera do Ignite"
-        onPress={() => console.log("teste")}
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
       />
     </Container>
   );
