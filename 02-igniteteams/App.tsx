16e42e6d8a2f8fc68b51fbea3c1f9c817c07dc37
goia <frnnd.goia97@gmail.com>
2024-10-22 14:31:08 -0300

    Adding custom fonts

--- 02-igniteteams/App.tsx
+++ 02-igniteteams/App.tsx
@@ -1,11 +1,25 @@
+import {
+  Roboto_400Regular,
+  Roboto_700Bold,
+  useFonts,
+} from "@expo-google-fonts/roboto";
 import { Groups } from "@screens/Groups";
-import { ThemeProvider } from "styled-components";
 import theme from "@theme/index";
+import { ActivityIndicator } from "react-native";
+import { ThemeProvider } from "styled-components";
+
+// Instalar fontes customizadas: npx expo install @expo-google-fonts/roboto expo-font -- --legacy-peer-deps
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      {fontsLoaded && <Groups />}
+      {!fontsLoaded && <ActivityIndicator />}
     </ThemeProvider>
   );
 }
