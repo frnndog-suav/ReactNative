23d92c8bbf4e684df02f7ae2ca451ed28e30976f
goia <frnnd.goia97@gmail.com>
2024-10-22 15:08:20 -0300

    Installing icon lib

--- 02-igniteteams/src/components/Header/index.tsx
+++ 02-igniteteams/src/components/Header/index.tsx
@@ -1,9 +1,18 @@
-import { Container, Logo } from "./styles";
 import LogoImg from "@assets/logo.png";
+import { BackButton, BackIcon, Container, Logo } from "./styles";
 
-export function Header() {
+type THeaderProps = {
+  showBackButton?: boolean;
+};
+
+export function Header({ showBackButton = false }: THeaderProps) {
   return (
     <Container>
+      {showBackButton && (
+        <BackButton>
+          <BackIcon />
+        </BackButton>
+      )}
       <Logo source={LogoImg} />
     </Container>
   );
