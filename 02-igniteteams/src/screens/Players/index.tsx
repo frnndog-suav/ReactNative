8baf90334a8b8e71707f94c08b48fb8474d249b8
goia <frnnd.goia97@gmail.com>
2024-10-23 17:53:10 -0300

    Displaying player cards

--- 02-igniteteams/src/screens/Players/index.tsx
+++ 02-igniteteams/src/screens/Players/index.tsx
@@ -1,16 +1,23 @@
+import { Button } from "@components/Button";
 import { ButtonIcon } from "@components/ButtonIcon";
 import { Filter } from "@components/Filter";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { Input } from "@components/Input";
+import { ListEmpty } from "@components/ListEmpty";
+import { PlayerCard } from "@components/PlayerCard";
 import { useState } from "react";
 import { FlatList } from "react-native";
 import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
-import { PlayerCard } from "@components/PlayerCard";
 
 export function Players() {
   const [team, setTeam] = useState("Time A");
-  const [players, setPlayers] = useState(["Teste", "Goia"]);
+  const [players, setPlayers] = useState([
+    "Teste",
+    "Goia",
+    "Barcelona",
+    "PLanet Coaster",
+  ]);
 
   return (
     <Container>
@@ -44,10 +51,18 @@ export function Players() {
       <FlatList
         data={players}
         keyExtractor={(item) => item}
+        ListEmptyComponent={<ListEmpty message="Não há pessoas nesse time." />}
+        showsVerticalScrollIndicator={false}
         renderItem={({ item }) => (
           <PlayerCard name={item} onRemove={() => {}} />
         )}
+        contentContainerStyle={[
+          { paddingBottom: 100 },
+          players.length === 0 && { flex: 1 },
+        ]}
       />
+
+      <Button title="Remover turma" type="SECONDARY" />
     </Container>
   );
 }
