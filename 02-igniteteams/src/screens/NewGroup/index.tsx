4a70df31964ac2eb38ab1ef6f436cebb9878eb40
goia <frnnd.goia97@gmail.com>
2024-10-26 14:47:19 -0300

    Treating exceptions

--- 02-igniteteams/src/screens/NewGroup/index.tsx
+++ 02-igniteteams/src/screens/NewGroup/index.tsx
@@ -3,9 +3,11 @@ import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { Input } from "@components/Input";
 import { useNavigation } from "@react-navigation/native";
-import { useState } from "react";
-import { Container, Content, Icon } from "./styles";
 import { groupCreate_AS } from "@storage/group/groupCreate";
+import { AppError } from "@utils/AppError";
+import { useState } from "react";
+import { Alert } from "react-native";
+import { Container, Content, Icon } from "./styles";
 
 export function NewGroup() {
   const navigation = useNavigation();
@@ -13,10 +15,20 @@ export function NewGroup() {
 
   async function handleNew() {
     try {
+      if (group.trim().length === 0)
+        return Alert.alert("Novo grupo", "Informe o nome da turma.");
+
       await groupCreate_AS(group);
+
       navigation.navigate("players", { group });
     } catch (error) {
-      console.log("error", error);
+      if (error instanceof AppError) {
+        Alert.alert("Novo grupo", error.message);
+        return;
+      }
+
+      Alert.alert("Novo grupo", "Não foi possível criar um novo grupo.");
+      console.log("GENERIC error: ", error);
     }
   }
 
