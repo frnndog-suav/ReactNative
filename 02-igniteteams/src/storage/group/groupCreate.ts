817313a736205a1021eedb3363409208e307e16e
goia <frnnd.goia97@gmail.com>
2024-10-26 14:37:33 -0300

    Implementing basic structure for errors

--- 02-igniteteams/src/storage/group/groupCreate.ts
+++ 02-igniteteams/src/storage/group/groupCreate.ts
@@ -1,10 +1,17 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { GROUP_COLLECTION } from "@storage/storageConfig";
+import { AppError } from "@utils/AppError";
 import { groupsGetAll_AS } from "./groupsGetAll";
 
 export async function groupCreate_AS(newGroup: string) {
   try {
     const storedGroups = await groupsGetAll_AS();
+
+    const groupAlreadyExists = storedGroups.includes(newGroup);
+
+    if (groupAlreadyExists) {
+      throw new AppError("Já existe um grupo cadastrado com esse nome.");
+    }
 
     const storage = JSON.stringify([...storedGroups, newGroup]);
 
