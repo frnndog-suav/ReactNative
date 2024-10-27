a821220ecbd28cf6fe10fac8b6b6a5ba8fdfa1d1
goia <frnnd.goia97@gmail.com>
2024-10-26 22:30:55 -0300

    Setting up gluestack lib

--- 03-ignite-gym/App.tsx
+++ 03-ignite-gym/App.tsx
@@ -1,26 +1,33 @@
 import { Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
+import { GluestackUIProvider, Text } from "@gluestack-ui/themed";
 import { useFonts } from "expo-font";
-import { StatusBar, Text, View } from "react-native";
+import { StatusBar, View } from "react-native";
 
 export default function App() {
   const [isFontsLoaded] = useFonts({ Roboto_700Bold, Roboto_400Regular });
 
   if (!isFontsLoaded) {
     return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text>Carregando...</Text>
-      </View>
+      <GluestackUIProvider>
+        <View
+          style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+        >
+          <Text>Carregando...</Text>
+        </View>
+      </GluestackUIProvider>
     );
   }
 
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <GluestackUIProvider>
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+        <Text>Open up App.tsx to start working on your app!</Text>
+      </View>
+    </GluestackUIProvider>
   );
 }
--- 03-ignite-gym/README.md
+++ 03-ignite-gym/README.md
@@ -1,3 +1,5 @@
 1 - Configurando os "paths" no "tsconfig.json"
 
 2 - Instalar fontes com `npx expo install @expo-google-fonts/roboto expo-font`
+
+3 - Instalar gluestack legado (para conseguir acompanhar o curso): `npm i @gluestack-ui/themed @gluestack-style/react react-native-svg@13.4.0 --legacy-peer-deps`
