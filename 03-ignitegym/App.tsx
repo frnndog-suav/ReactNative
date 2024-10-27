62eda002dbc958ae4a89a10cd70a79eea47c6b9e
goia <frnnd.goia97@gmail.com>
2024-10-26 21:42:21 -0300

    Fixing status bar

--- 03-ignitegym/App.tsx
+++ 03-ignitegym/App.tsx
@@ -3,8 +3,7 @@ import {
   Roboto_700Bold,
   useFonts,
 } from "@expo-google-fonts/roboto";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StatusBar, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   const [isFontsLoaded] = useFonts({ Roboto_700Bold, Roboto_400Regular });
@@ -19,8 +18,12 @@ export default function App() {
 
   return (
     <View style={styles.container}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
       <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
