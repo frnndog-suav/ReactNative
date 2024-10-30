4ff6d974c3cc5304be8f2474056ab1c7f71db5d0
goia <frnnd.goia97@gmail.com>
2024-10-29 22:32:08 -0300

    Small fix in login screen

--- 03-ignite-gym/src/screens/SignIn/index.tsx
+++ 03-ignite-gym/src/screens/SignIn/index.tsx
@@ -2,46 +2,59 @@ import BackgroundImg from "@assets/background.png";
 import Logo from "@assets/logo.svg";
 import { Button } from "@components/Button";
 import { Input } from "@components/Input";
-import { Center, Heading, Image, Text, VStack } from "@gluestack-ui/themed";
+import {
+  Center,
+  Heading,
+  Image,
+  ScrollView,
+  Text,
+  VStack,
+} from "@gluestack-ui/themed";
 
 export function SignIn() {
   return (
-    <VStack flex={1} backgroundColor="#121214" alignItems="center">
-      <Image
-        alt="Pessoas treinando"
-        source={BackgroundImg}
-        defaultSource={BackgroundImg}
-        style={{ width: "100%", height: 624 }}
-        position="absolute"
-      />
-
-      <Center marginTop={154} marginBottom={60}>
-        <Logo />
-        <Text color="#E1E1E6" fontSize={16}>
-          Treine sua mente e seu corpo
-        </Text>
-      </Center>
-
-      <Center gap={16} justifyContent="center">
-        <Heading color="#E1E1E6" alignSelf="center">
-          Acesse a conta
-        </Heading>
-        <Input
-          placeholder="E-mail"
-          keyboardType="email-address"
-          autoCapitalize="none"
+    <ScrollView flex={1} backgroundColor="#121214">
+      <VStack
+        minHeight={"100%"}
+        alignItems="center"
+        justifyContent="space-between"
+      >
+        <Image
+          alt="Pessoas treinando"
+          source={BackgroundImg}
+          defaultSource={BackgroundImg}
+          style={{ width: "100%", height: 700 }}
+          position="absolute"
         />
-        <Input placeholder="Senha" secureTextEntry />
 
-        <Button title="Acessar" />
-      </Center>
+        <Center marginTop={154} marginBottom={60}>
+          <Logo />
+          <Text color="#E1E1E6" fontSize={16}>
+            Treine sua mente e seu corpo
+          </Text>
+        </Center>
 
-      <Center flex={1} justifyContent="flex-end" marginBottom={40}>
-        <Text color="#E1E1E6" fontSize={14} marginBottom={16} >
-          Ainda não tem acesso?
-        </Text>
-        <Button title="Criar conta" variant="outlined" />
-      </Center>
-    </VStack>
+        <Center gap={16} justifyContent="center">
+          <Heading color="#E1E1E6" alignSelf="center">
+            Acesse a conta
+          </Heading>
+          <Input
+            placeholder="E-mail"
+            keyboardType="email-address"
+            autoCapitalize="none"
+          />
+          <Input placeholder="Senha" secureTextEntry />
+
+          <Button title="Acessar" />
+        </Center>
+
+        <Center flex={1} justifyContent="flex-end" marginBottom={40}>
+          <Text color="#E1E1E6" fontSize={14} marginBottom={16}>
+            Ainda não tem acesso?
+          </Text>
+          <Button title="Criar conta" variant="outlined" />
+        </Center>
+      </VStack>
+    </ScrollView>
   );
 }
