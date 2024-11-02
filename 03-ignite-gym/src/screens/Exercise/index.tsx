2d695856f17d752237a353ffa4f25c2275d1c719
goia <frnnd.goia97@gmail.com>
2024-11-02 15:40:07 -0300

    Adding styles in exercise details screen header

--- 03-ignite-gym/src/screens/Exercise/index.tsx
+++ 03-ignite-gym/src/screens/Exercise/index.tsx
@@ -1,4 +1,5 @@
-import { Icon, VStack } from "@gluestack-ui/themed";
+import BodySvg from "@assets/body.svg";
+import { Heading, HStack, Icon, Text, VStack } from "@gluestack-ui/themed";
 import { useNavigation } from "@react-navigation/native";
 import { AppNavigatorRoutesProps } from "@routes/app.routes";
 import { ArrowLeft } from "lucide-react-native";
@@ -6,10 +7,10 @@ import { TouchableOpacity } from "react-native";
 import { MY_THEME_CONTROLLER } from "../../theme";
 
 export function Exercise() {
-  const navigation = useNavigation<AppNavigatorRoutesProps>()
+  const navigation = useNavigation<AppNavigatorRoutesProps>();
 
   function handleGoBack() {
-    navigation.goBack()
+    navigation.goBack();
   }
 
   return (
@@ -17,7 +18,7 @@ export function Exercise() {
       <VStack
         paddingHorizontal={32}
         backgroundColor={MY_THEME_CONTROLLER.COLORS.GRAY_600}
-        paddingTop={48}
+        paddingTop={60}
       >
         <TouchableOpacity onPress={handleGoBack}>
           <Icon
@@ -27,6 +28,34 @@ export function Exercise() {
             height={80}
           />
         </TouchableOpacity>
+
+        <HStack
+          display="flex"
+          flexDirection="row"
+          justifyContent="space-between"
+          alignItems="center"
+          marginTop={16}
+          marginBottom={16}
+        >
+          <Heading
+            color={MY_THEME_CONTROLLER.COLORS.GRAY_100}
+            fontFamily={MY_THEME_CONTROLLER.FONTS.HEADING}
+            fontSize={MY_THEME_CONTROLLER.FONT_SIZE.LG}
+            flexShrink={1}
+          >
+            Puxada frontal
+          </Heading>
+          <HStack display="flex" flexDirection="row" alignItems="center">
+            <BodySvg />
+            <Text
+              color={MY_THEME_CONTROLLER.COLORS.GRAY_200}
+              marginLeft={4}
+              textTransform="capitalize"
+            >
+              Costas
+            </Text>
+          </HStack>
+        </HStack>
       </VStack>
     </VStack>
   );
