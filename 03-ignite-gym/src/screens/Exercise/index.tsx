92f3951a6edca1bd27dc05a7645caaebfc499098
goia <frnnd.goia97@gmail.com>
2024-11-02 15:33:39 -0300

    Initializing exercise details screen

--- 03-ignite-gym/src/screens/Exercise/index.tsx
+++ 03-ignite-gym/src/screens/Exercise/index.tsx
@@ -1,9 +1,33 @@
-import { Center, Text } from "@gluestack-ui/themed";
+import { Icon, VStack } from "@gluestack-ui/themed";
+import { useNavigation } from "@react-navigation/native";
+import { AppNavigatorRoutesProps } from "@routes/app.routes";
+import { ArrowLeft } from "lucide-react-native";
+import { TouchableOpacity } from "react-native";
+import { MY_THEME_CONTROLLER } from "../../theme";
 
 export function Exercise() {
+  const navigation = useNavigation<AppNavigatorRoutesProps>()
+
+  function handleGoBack() {
+    navigation.goBack()
+  }
+
   return (
-    <Center flex={1}>
-      <Text color="white">Exercise</Text>
-    </Center>
+    <VStack flex={1}>
+      <VStack
+        paddingHorizontal={32}
+        backgroundColor={MY_THEME_CONTROLLER.COLORS.GRAY_600}
+        paddingTop={48}
+      >
+        <TouchableOpacity onPress={handleGoBack}>
+          <Icon
+            as={ArrowLeft}
+            color={MY_THEME_CONTROLLER.COLORS.GREEN_500}
+            width={80}
+            height={80}
+          />
+        </TouchableOpacity>
+      </VStack>
+    </VStack>
   );
 }
