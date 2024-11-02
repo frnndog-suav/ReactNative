a15499197ac39bfcf77c4a44d6259bd51e556262
goia <frnnd.goia97@gmail.com>
2024-11-02 12:02:06 -0300

    Implementing user image component

--- 03-ignite-gym/src/screens/Home/components/HomeHeader/index.tsx
+++ 03-ignite-gym/src/screens/Home/components/HomeHeader/index.tsx
@@ -1,13 +1,23 @@
 import { Heading, HStack, Text, VStack } from "@gluestack-ui/themed";
+import { UserPhoto } from "../UserPhoto";
 
 export function HomeHeader() {
   return (
     <HStack
+      display="flex"
+      flexDirection="row"
       paddingTop={64}
       paddingBottom={14}
       paddingHorizontal={32}
       backgroundColor="#202024"
+      gap={8}
     >
+      <UserPhoto
+        source={{ uri: "https://github.com/frnndog-suav.png" }}
+        alt="Imagem do usuário"
+        width={64}
+        height={64}
+      />
       <VStack>
         <Text color="#E1E1E6" fontSize={14}>
           Olá,
