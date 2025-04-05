0a33fa3a96a2d3245c842fbbf90ba5443fc3f086
goia <frnnd.goia97@gmail.com>
2025-04-05 13:57:04 -0300

    Implementing failed requests queue

--- 04-ignite-gym-with-backend/src/services/api.ts
+++ 04-ignite-gym-with-backend/src/services/api.ts
@@ -1,12 +1,20 @@
 import { storageAuthTokenGet } from "@storage/storageAuthToken";
 import { AppError } from "@utils/appError";
-import axios, { AxiosInstance } from "axios";
+import axios, { AxiosError, AxiosInstance } from "axios";
+
+type TPromiseType = {
+  onSuccess: (token: string) => void;
+  onFailure: (error: AxiosError) => void;
+};
 
 type TSignOut = () => void;
 
 type TApiInstanceProps = AxiosInstance & {
   registerInterceptTokenManager: (signOut: TSignOut) => () => void;
 };
+
+const failedQueue: TPromiseType[] = [];
+let isRefreshing = false;
 
 const api = axios.create({
   baseURL: "http://192.168.15.105:3333",
@@ -27,6 +35,26 @@ api.registerInterceptTokenManager = (signOut) => {
             signOut();
             return Promise.reject(requestError);
           }
+
+          const originalRequestConfig = requestError.config;
+
+          if (isRefreshing) {
+            return new Promise((resolve, reject) => {
+              failedQueue.push({
+                onSuccess: (token) => {
+                  originalRequestConfig.headers = {
+                    Authorization: `Bearer ${token}`,
+                  };
+                  resolve(api(originalRequestConfig));
+                },
+                onFailure: (error) => {
+                  reject(error);
+                },
+              });
+            });
+          }
+
+          isRefreshing = true;
         }
 
         signOut();
