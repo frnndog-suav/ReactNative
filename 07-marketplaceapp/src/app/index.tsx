272b439cdeaafa1f94788f2d2db36cda9ad693de
Fernando <frnnd.goia97@gmail.com>
2025-11-14 14:03:03 -0300

    chore: setting up private routes

--- 07-marketplaceapp/src/app/index.tsx
+++ 07-marketplaceapp/src/app/index.tsx
@@ -1,5 +1,14 @@
 import { Redirect } from "expo-router";
 
 export default function App() {
+  const userData = {
+    token: "tokenteste",
+    name: "Usuário Teste",
+  };
+
+  if (userData) {
+    return <Redirect href={"/(private)/home"} />;
+  }
+
   return <Redirect href={"/login"} />;
 }
