9db3061e907ca4b35aafeae46f953c39a7668ed0
Fernando <frnnd.goia97@gmail.com>
2025-11-16 13:54:53 -0300

    chore: adding register form schema

---
+++ 07-marketplaceapp/src/app/viewModels/Register/register.schema.ts
@@ -0,0 +1,18 @@
+import * as yup from "yup";
+
+export const registerSchema = yup.object({
+  name: yup
+    .string()
+    .required("Nome é obrigatório!")
+    .min(3, "Nome deve ter pelo menos 3 caracteres"),
+  email: yup.string().email("Email inválido").required("Email é obrigatório!"),
+  password: yup.string().required("Senha é obrigatória!"),
+  confirmPassword: yup
+    .string()
+    .required("Senha é obrigatória!")
+    .oneOf([yup.ref("password")], "Senhas não coincidem."),
+  phone: yup
+    .string()
+    .required("Telefone é obrigatório!")
+    .matches(/^\d{11}$/, "Telefone deve ter 11 dígitos (DDD + número)."),
+});
