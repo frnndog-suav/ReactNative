88457fd0e24a307f37ff119f946a2b6dc71dd65c
Fernando <frnnd.goia97@gmail.com>
2025-11-18 18:41:21 -0300

    refactor: configuring styles on input

--- 07-marketplaceapp/src/app/shared/components/AppInput/index.tsx
+++ 07-marketplaceapp/src/app/shared/components/AppInput/index.tsx
@@ -1,21 +1,47 @@
 import { Ionicons } from "@expo/vector-icons";
-import { Pressable, TextInput, TouchableOpacity, View } from "react-native";
-import { appInputVariants } from "./index.variants";
+import { FC } from "react";
+import {
+  Pressable,
+  Text,
+  TextInput,
+  TextInputProps,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { appInputVariants, TAppInputVariantsProps } from "./index.variants";
 
-export function AppInput() {
+type TProps = TextInputProps &
+  TAppInputVariantsProps & {
+    label?: string;
+    containerClassName?: string;
+    mask?: (value: string) => void | string;
+    leftIcon?: keyof typeof Ionicons.glyphMap;
+    rightIcon?: keyof typeof Ionicons.glyphMap;
+  };
+
+export const AppInput: FC<TProps> = ({
+  mask,
+  label,
+  leftIcon,
+  rightIcon,
+  className,
+  containerClassName,
+  ...textInputProps
+}) => {
   const styles = appInputVariants({});
 
   return (
-    <View>
-      <Pressable>
-        <Ionicons name="person" />
+    <View className={styles.container({ className: containerClassName })}>
+      <Text className={styles.label()}>Label</Text>
+      <Pressable className={styles.wrapper()}>
+        <Ionicons size={22} name="person" />
 
-        <TextInput />
+        <TextInput className={styles.input()} {...textInputProps} />
 
         <TouchableOpacity>
-          <Ionicons name="eye-off-outline" />
+          <Ionicons size={22} name="eye-off-outline" />
         </TouchableOpacity>
       </Pressable>
     </View>
   );
-}
+};
--- 07-marketplaceapp/src/app/shared/components/AppInput/index.variants.ts
+++ 07-marketplaceapp/src/app/shared/components/AppInput/index.variants.ts
@@ -1,12 +1,12 @@
-import { tv } from "tailwind-variants";
+import { tv, VariantProps } from "tailwind-variants";
 
 export const appInputVariants = tv({
   slots: {
     container: "w-full my-4",
     error: "text-sm text-danger mt-1",
-    label: "text-xs text-gray-300 mb-1 font-semibold",
+    label: "text-xs text-gray-300 mb-3 font-semibold",
     input: "bg-transparent text-gray-500 text-base flex-1",
-    wrapper: "flex-row items-center border-b border-gray-200 pb-5",
+    wrapper: "flex-row items-center border-b border-gray-200 pb-2",
   },
   variants: {
     isFocused: {
@@ -25,3 +25,5 @@ export const appInputVariants = tv({
     isDisabled: false,
   },
 });
+
+export type TAppInputVariantsProps = VariantProps<typeof appInputVariants>;
